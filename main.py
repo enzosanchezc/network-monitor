6d7f1373d8ab5be294173d9ae334479b7eb1de63
Enzo Sanchez Croce <enzosanchezcroce@hotmail.com>
2023-03-06 14:17:09 -0300

    Added influxdb connection

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import datetime
 import nmap
 import sqlite3
+import influxdb
 import subprocess
 import time
 import os
@@ -142,3 +144,62 @@ def log_connection_changes():
 update_active_devices()
 log_connection_changes()
 
+# insert active_devices into influx measument named "active_devices"
+influx_host = os.getenv("INFLUX_HOST") or "localhost"
+influx_port = int(os.getenv("INFLUX_PORT") or int(8086))
+influx_db = os.getenv("INFLUX_DB") or "network_monitor"
+
+influx_client = influxdb.InfluxDBClient(host=influx_host, port=influx_port, database=influx_db)
+influx_client.create_database(influx_db)
+
+# query the active_devices table
+c.execute("SELECT * FROM active_devices")
+active_devices = c.fetchall()
+
+# iterate through each active device
+for active_device in active_devices:
+    # create a dictionary for the influxdb point
+    point = {
+        "measurement": "active_devices",
+        "tags": {
+            "mac": active_device[0],
+            "ip": active_device[1],
+            "hostname": active_device[2],
+            "network": network
+        },
+        "time": active_device[3] * 1000000000,
+        "fields": {
+            "last_seen": datetime.datetime.fromtimestamp(active_device[3]).strftime('%Y-%m-%d %H:%M:%S'),
+            "first_seen": datetime.datetime.fromtimestamp(active_device[4]).strftime('%Y-%m-%d %H:%M:%S')
+        }
+    }
+    # write the point to influxdb
+    influx_client.write_points([point])
+
+# insert connection_log into influx measument named "connection_log"
+# query the connection_log table
+c.execute("SELECT * FROM connection_log")
+connection_log = c.fetchall()
+
+# iterate through each connection log entry
+for log_entry in connection_log:
+    # create a dictionary for the influxdb point
+    point = {
+        "measurement": "connection_log",
+        "tags": {
+            "mac": log_entry[0],
+            "hostname": log_entry[1],
+            "connection_status": log_entry[2],
+            "network": network
+        },
+        "time": log_entry[3] * 1000000000,
+        "fields": {
+            "timestamp": datetime.datetime.fromtimestamp(log_entry[3]).strftime('%Y-%m-%d %H:%M:%S')
+        }
+    }
+
+    # write the point to influxdb
+    influx_client.write_points([point])
+
+# close the database connection
+conn.close()
