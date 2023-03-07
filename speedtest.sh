22559a0f0aef3b73bf239d46e44bda7a1f970986
Enzo Sanchez Croce <enzosanchezcroce@hotmail.com>
2023-03-07 12:39:49 -0300

    Added speedtest script

---
+++ speedtest.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ -f .env ]; then
+  export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
+fi
+
+FILENAME=last_speedtest.log
+
+speedtest > $FILENAME
+
+PROVIDER=$(grep "Testing from" $FILENAME | cut -d" " -f3- | cut -d"(" -f1 | xargs)
+PUBLIC_IP=$(grep "Testing from" $FILENAME | cut -d"(" -f2 | cut -d")" -f1 | xargs)
+SPEEDTEST_SERVER=$(grep "Hosted by" $FILENAME | cut -d" " -f3- | cut -d"[" -f1 | xargs)
+SERVER_DISTANCE=$(grep "Hosted by" $FILENAME | cut -d"[" -f2 | cut -d" " -f1 | xargs)
+PING=$(grep "Hosted by" $FILENAME | cut -d":" -f2 | cut -d" " -f2 | xargs)
+DOWNLOAD=$(grep "Download" $FILENAME | cut -d" " -f2 | xargs)
+UPLOAD=$(grep "Upload" $FILENAME | cut -d" " -f2 | xargs)
+
+if ! curl -s "http://$INFLUX_HOST:$INFLUX_PORT/query?q=SHOW+DATABASES" | jq '.results[0].series[0].values[][]' | grep $INFLUX_DB > /dev/null; then
+	curl -s "http://$INFLUX_HOST:$INFLUX_PORT/query?q=CREATE+DATABASE+$INFLUX_DB"
+	echo "Influx DB created"
+fi
+
+curl -s -XPOST "http://$INFLUX_HOST:$INFLUX_PORT/write?db=$INFLUX_DB" --data-binary "speedtest provider=\"$PROVIDER\",ip=\"$PUBLIC_IP\",server=\"$SPEEDTEST_SERVER\",distance=$SERVER_DISTANCE,latency=$PING,download=$DOWNLOAD,upload=$UPLOAD"
+
+echo "Point inserted:"
+echo "PROVIDER=$PROVIDER"
+echo "PUBLIC_IP=$PUBLIC_IP"
+echo "SPEEDTEST_SERVER=$SPEEDTEST_SERVER"
+echo "SERVER_DISTANCE=$SERVER_DISTANCE"
+echo "PING=$PING"
+echo "DOWNLOAD=$DOWNLOAD"
+echo "UPLOAD=$UPLOAD"
